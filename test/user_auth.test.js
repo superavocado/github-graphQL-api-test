cec4a3f1d0845abb91d0fbeba0599364c20c8a16
superavocado <okiukun@gmail.com>
2019-07-24 20:44:03 +1000

    query repos

---
+++ test/user_auth.test.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+const axios = require('axios');
+headers = { Authorization: process.env.AUTH }
+
+const queryListRepos =
+`query {
+  viewer {
+    repositories(first:1) {
+      nodes {
+        name
+      }
+    }
+  }
+}`;
+
+describe('test', () => {
+  it('test', async () => {
+    let res = await axios({
+      method: "post",
+      url: "https://api.github.com/graphql",
+      //data: { query: 'query {repository(owner:"test007-github", name:"monkey"){name}}'},
+      //data: { query: 'query {viewer{repository(name:"monkey"){name id}}}'},
+      data: { query:  queryListRepos},
+      headers: headers
+    });
+    console.log(JSON.stringify(res.data, null, 2));
+    expect(res.status).to.equal(200);
+
+  })
+})
